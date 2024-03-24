fe13927bd91b25b9f5678fa410c42a2808cb5945
tzahbak <tzahi.bakal@gmail.com>
2024-03-24 19:37:24 +0200

    test

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
 
 function performSearch() {
     var searchString = document.getElementById('searchInput').value;
